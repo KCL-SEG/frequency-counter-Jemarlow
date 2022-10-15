d81af19b18b45f4818a3d4774de1041134272890
Jmarlow <45525109+Jemarlow@users.noreply.github.com>
2022-10-16 00:44:07 +0100

    fix for mixed lists

--- frequencies.py
+++ frequencies.py
@@ -5,7 +5,12 @@ def frequencies(items):
     
     frequencies = {}
 
-    for item in list(set(items)): # convert items to set and count each occurrence
-        frequencies[item] = items.count(item)
+    for item in items:
+        item = str(item)
+        if (item in frequencies):
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1
+
 
     return frequencies
